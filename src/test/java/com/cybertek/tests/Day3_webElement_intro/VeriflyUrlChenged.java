36c12a83f78fd4f5097ef928fc484d3502e16dc7
“Nijat” <tursunnijat@hotmail.com>
2019-11-03 21:27:15 +0100

    day3 test case

---
+++ src/test/java/com/cybertek/tests/Day3_webElement_intro/VeriflyUrlChenged.java
@@ -0,0 +1,51 @@
+package com.cybertek.tests.Day3_webElement_intro;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class VeriflyUrlChenged {
+    /**
+     * Verify URL changed
+     * open browser
+     * go to http://practice.cybertekschool.com/forgot_passwordLinks to an external site.
+     * enter any email
+     * click on Retrieve password
+     * verify that url changed to http://practice.cybertekschool.com/email_sent
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // open browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // * go to http://practice.cybertekschool.com/forgot_passwordLinks to an external site.
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+
+
+
+        //* enter any email
+        WebElement inputBox = driver.findElement(By.name("email"));
+        inputBox.sendKeys("tursunnijat@gmail.com");
+
+        //     /* c lick on Retrieve password
+        WebElement retrievePasswordButtom = driver.findElement(By.id("form_submit"));
+        retrievePasswordButtom.click();
+
+        // * verify that url changed to http://practice.cybertekschool.com/email_sent
+        String expectedUrl = " http://practice.cybertekschool.com/email_sent";
+
+        // * save url after clicking button
+        String actualUrl = driver.getCurrentUrl();
+
+        // * verify that url did not change
+        if(expectedUrl.equals(actualUrl)){
+            System.out.println("pass");
+        }else{
+            System.out.println("fail");
+        }
+
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/cybertek/tests/Day3_webElement_intro/VerifyURLnotchanged.java
@@ -0,0 +1,49 @@
+package com.cybertek.tests.Day3_webElement_intro;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class VerifyURLnotchanged {
+
+    /**
+     * Verify URL not changed
+     * open browser
+     * go to http://practice.cybertekschool.com/forgot_password Links to an external site.
+     * click on Retrieve password
+     * verify that url did not change
+     *
+     */
+
+
+    public static void main(String[] args) throws InterruptedException {
+        //   * open browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        //  * go to http://practice.cybertekschool.com/forgot_password Links to an external site.
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+
+        // save url to string variable
+        String expectedUrl = driver.getCurrentUrl();
+
+        //  * click on Retrieve password
+        WebElement retrievePasswordButtom = driver.findElement(By.id("form_submit"));
+        retrievePasswordButtom.click();
+
+        // * save url after clicking button
+        String actualUrl = driver.getCurrentUrl();
+
+        // * verify that url did not change
+        if(expectedUrl.equals(actualUrl)){
+            System.out.println("Pass");
+        }else{
+            System.out.println("fail");
+        }
+
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
